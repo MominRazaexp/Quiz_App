beaa293dafd3fc1b36985f9c1621b8787f123487
MominExpertizo <momin.raza@expertizo.pk>
2025-05-21 12:05:01 +0500

    style: talwind correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,9 @@
-'use client';
-import { useState, ChangeEvent } from 'react';
-import { useRouter } from 'next/navigation';
-
+import HomeView from "@/view/HomeView";
 export default function Home() {
-  const router = useRouter();
-  const [name, setName] = useState('');
-
-  const handleStartQuiz = (): void => {
-    if (name.trim() === '') {
-      alert('Please enter your name before starting the quiz.');
-      return;
-    }
-    router.push('/questions');
-  };
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setName(e.target.value);
-  };
-
-  return (
-    <div className="flex flex-col justify-center items-center h-screen ">
-      <div className='flex flex-col justify-center items-center px-4 py-4 rounded-lg drop-shadow-lg shadow-2xl border-gray-200 border-2'>
-        <h1 className="m-5 text-xl font-md">Welcome to our Quiz App</h1>
-      <input
-        className="w-80 p-2 rounded mb-10 placeholder-gray-500 text-gray-500 border-gray-200 border-2"
-        type="text"
-        placeholder="Enter Your Name"
-        value={name}
-        onChange={handleInputChange}
-      />
-      <button
-        onClick={handleStartQuiz}
-        className="bg-white py-2 px-3 rounded text-black border-gray-200 border-2"
-      >
-        Start Quiz
-      </button>
-      </div>
-      
+  return(
+    <div>
+      <HomeView/>
     </div>
-  );
+  )
+
 }
