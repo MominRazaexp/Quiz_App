09263922696eea55784f2a4fc1f395c1de202fb1
Momin Raza <momin.raza@expertizo.pk>
2025-05-23 00:24:45 +0500

    fix: choices random logic

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChoiceInterface } from "../Interface/interface";
+
+function shuffleArray<T>(array: T[]): T[] {
+  return [...array].sort(() => Math.random() - 0.5);
+}
 
 export default function Choice({
   choices,
-  isAnswerd,
+  isAnswered,
   handleAnswer,
   selectedChoice,
 }: ChoiceInterface) {
+  const [shuffledChoices, setShuffledChoices] = useState<string[]>(
+    shuffleArray(choices)
+  );
+  useEffect(() => {
+    setShuffledChoices(shuffleArray(choices));
+  }, [choices]);
+
   return (
     <div className="flex flex-wrap justify-center sm:justify-between mt-5">
-      {choices.map((choice, index) => {
+      {shuffledChoices.map((choice, index) => {
         return (
           <div
             key={index}
             onClick={() => {
-              if (!isAnswerd) {
+              if (!isAnswered) {
                 handleAnswer(choice);
               }
             }}
             className={`${
-              isAnswerd ? "cursor-not-allowed" : "cursor-pointer"
+              isAnswered ? "cursor-not-allowed" : "cursor-pointer"
             } text-center w-[70%] px-2 py-1 sm:w-[48%] sm:px-5 text-xs sm:text-base sm:py-2 mb-2 sm:mb-4 rounded-md 
-                  ${
-                    isAnswerd && selectedChoice === choice
-                      ? "bg-black text-white"
-                      : isAnswerd && selectedChoice !== choice
-                      ? "opacity-50"
-                      : "bg-gray-400"
-                  }
-                   border-black border-2 `}
+              ${
+                isAnswered && selectedChoice === choice
+                  ? "bg-black text-white"
+                  : isAnswered && selectedChoice !== choice
+                  ? "opacity-50"
+                  : "bg-gray-400"
+              }
+              border-black border-2 `}
           >
             {choice}
           </div>
