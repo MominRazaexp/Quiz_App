dee9f9ef43000e0ae7f519fce4fb331a41e9212f
Momin Raza <momin.raza@expertizo.pk>
2025-05-23 00:23:07 +0500

    style: responsive result card

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -9,14 +9,14 @@ export default function ResultCard({
   handleStartQuiz,
 }: ResultInterface) {
   return (
-    <div className="bg-white rounded-lg px-2 flex flex-col items-center  text-black w-[20%] py-4  shadow-2xl border-gray-200 border-2">
-      <h1 className=" text-xl font-bold">Here is Your Score</h1>
-      <span className="text-lg mt-10">{`You have Corrected ${corrected} out of ${totalQ}`}</span>
-      <span className="text-lg mt-3">{`Percentage: ${(
+    <div className="px-4 py-4 bg-white rounded-lg  flex flex-col items-center  text-black  shadow-2xl border-gray-200 border-2">
+      <h1 className=" text-sm sm:text-xl font-bold">Here is Your Score</h1>
+      <span className="text-sm sm:text-lg mt-10">{`You have Corrected ${corrected} out of ${totalQ}`}</span>
+      <span className="text-sm sm:text-lg mt-3">{`Percentage: ${(
         (corrected / totalQ) *
         100
       ).toFixed(0)}%`}</span>
-      <div className="flex justify-around w-[100%] mt-5 ">
+      <div className="flex gap-3 justify-around mt-5 ">
         <Button btnAction={handleStartQuiz} btnName="Start New Quiz" />
         <Button btnAction={handleReStartQuiz} btnName="Restart Quiz" />
       </div>
